587bfca19efd8e1337aee4cdd42f723df176a965
hongjun500 <bhj.hong@qq.com>
2021-10-05 15:54:58 +0800

    包,方法引用,入参，返回值

--- chapter01/main.go
+++ chapter01/main.go
@@ -4,10 +4,10 @@ package main
 // 单个导入依赖包
 import "fmt"
 
+// 分组导入多个(推荐)
 import (
+	"math"
 	"time"
-	// 导入但不引用改包的函数,即忽略
-	_ "math"
 )
 
 /**
@@ -21,4 +21,38 @@ func main() {
 	fmt.Println(time.Now())
 
 	fmt.Println(time.Hour)
+
+	fmt.Println(math.Pi)
+	// 报错，小写字母开头未导出(即为无法引用)
+	// 不同包内小写开头无法导入
+	// fmt.Println(math.pi)
+
+	fmt.Println(add(1, 1))
+
+	// 有参无返回值
+	logNoReturn("hello, go")
+
+	// 无参且无返回值
+	logNoReturnAndParam()
+}
+
+/**
+两个int类型入参，返回一个int类型参数
+*/
+func add(x, y int) int {
+	return x + y
+}
+
+/**
+一个string类型入参并打印，无法返回值
+*/
+func logNoReturn(str string) {
+	fmt.Println(str)
+}
+
+/**
+无参数无返回值的打印
+*/
+func logNoReturnAndParam() {
+	fmt.Println("无参数无返回值的打印")
 }
