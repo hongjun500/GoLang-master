3ce432149378fba2149f5882101ff120fe920fa2
hongjun500 <bhj.hong@qq.com>
2023-08-18 17:21:09 +0800

    对函数的匿名函数及其闭包进行补充

--- chapter06/go.mod
+++ chapter06/go.mod
@@ -1,3 +1,3 @@
-module chapter06
+module github.com/hongjun500/GoLang-master/chapter06
 
 go 1.19
--- chapter06/main.go
+++ chapter06/main.go
@@ -9,7 +9,6 @@ import (
 const GO_LANG = "GOlANG"
 
 func main() {
-	Print("hello go")
 
 	fmt.Println(sum(1, 1))
 
@@ -20,51 +19,40 @@ func main() {
 	logNoReturnAndParam()
 
 	// 函数可以有多个参数并且有多个返回值
-	arr1, arr2, arr3 := moreReturn(1, 2, 3)
-	/*intArrays := make([]int, 3)
-	stringArrays := make([]string, 1)
-	intArrays[0] = arr1
-	intArrays[1] = arr2
-	stringArrays[0] = arr3
-	fmt.Println(intArrays, stringArrays)*/
+	a, b, c := moreReturn(1, 2, 3)
 
-	// 打印时可接受参数%v
-	fmt.Printf("返回的三个参数分别是%v,%v,%v \n", arr1, arr2, arr3)
+	// 打印时可接受参数 %v
+	fmt.Printf("返回的三个参数分别是%v,%v,%v \n", a, b, c)
 
 	// 匿名函数  (隐藏函数名)
-	var fc = func() int {
+	var fc = func(name string) int {
 		i := 0
 		i++
-		fmt.Println("匿名函数作为变量使用")
+		fmt.Printf("匿名函数接收到的参数是：%s\n", name)
 		return i
 	}
-	fmt.Println(fc())
+	fc("fc")
 
-	func() {
-		// func(){}()
-		fmt.Println("直接使用匿名函数")
-	}() /*这个小括号不能省略*/
+	str := "外部变量 str "
+	func() string {
+		fmt.Printf("直接使用匿名函数时引用外部变量=%v\n", str)
+		fmt.Printf("直接使用匿名函数时引用外部常量=%v\n", GO_LANG)
+		return str
+	}() // 这个小括号不能省略，代表调用此匿名函数
 
-	str := "外部变量str"
-	func() {
-		fmt.Printf("直接使用匿名函数引用外部变量=%v\n", str)
-		fmt.Printf("直接使用匿名函数引用外部常量=%v\n", GO_LANG)
-	}()
-
-	// defer关键字配合闭包使用
-
+	// defer 关键字配合闭包使用
 	strWord := "hello world"
-	defer func() { // 这里的defer关键字会使此匿名函数延迟调用, 并且strWord的内存地址会被defer关键字保留，而并不是保留其值的内容
-		fmt.Printf("外部变量strWord=%v \n", strWord)
+	defer func() { // 这里的 defer 关键字会使此匿名函数延迟调用, 并且 strWord 的内存地址会被匿名函数保留，而并不是保留其值
+		fmt.Printf("外部变量 strWord =%v \n", strWord) // hello goLang
 	}()
 
-	strWord = "hello goLang"             // 这里改变了变量strWord的值，内存地址并没有改变
-	fmt.Printf("strWord=%v \n", strWord) // 这里执行完之后匿名函数开始调用，记录的strWord的值就是改变之后的值
+	// 这里改变了变量 strWord 的值，内存地址并没有改变
+	strWord = "hello goLang"
 
 	// 函数式编程 (打印出main.div)
-	fmt.Println(func_div(div, 100, 2))
+	fmt.Println(funcDiv(div, 100, 2))
 	// 匿名函数， 打印出main.main.func(随机数命名值)
-	fmt.Println(func_div(func(i int, i2 int) (int, int) {
+	fmt.Println(funcDiv(func(i int, i2 int) (int, int) {
 		return i / i2, i % i2
 	}, 100, 2))
 
@@ -73,51 +61,34 @@ func main() {
 
 }
 
-// 函数关键字func 函数名pring, 入参为一个string类型，无返回值
-func Print(str string) {
-	fmt.Printf("传入了一个参数：%v \n", str)
-	fmt.Println(str)
-}
-
-/*
-一个string类型入参并打印，无法返回值
-*/
+// 有参无返回值
 func logNoReturn(str string) {
 	fmt.Println(str)
 }
 
-/*
-两个int类型入参，返回一个int类型参数
-*/
+// 两个 int 类型的参数并且返回一个 int 类型的值
 func sum(x, y int) int {
 	return x + y
 }
 
-/*
-无参数无返回值的打印
-*/
+// 无参无返回值
 func logNoReturnAndParam() {
-	fmt.Println("无参数无返回值的打印")
+	fmt.Println("无参数无返回值的函数")
 }
 
-/*
-接收三个参数，并且返回两个int类型一个string类型的值
-注意这里的返回值a,b,c被命名了
-返回值的命名应当具有一定含义，可以作为文档使用
-返回值命名时，返回最后可以直接return
-*/
+// moreReturn 接收三个参数，并且返回两个 int 类型一个 string 类型的值
+// 这里的返回值 a,b,c 被命名, 可以直接 return
+// 返回值的命名应当具有一定含义，可以作为文档使用
 func moreReturn(x, y, z int) (a, b int, c string) {
 	a = x + y + z
 	b = x * y * z
 	// return a, b, "string"
-	// 效果同上return,如果函数比较长这种方式可读性比较差
+	// 效果同上,如果函数比较长这种方式可读性比较差
 	return
 }
 
 // 对比上一个函数
 func moreReturnV1(x, y, z int) (int, int, string) {
-	// := 语句可以省略var声明
-	// 但是 := 只能在函数内声明，函数外只能以关键字开头
 	a := x + y + z
 	b := x * y * z
 	return a, b, "string"
@@ -127,7 +98,7 @@ func div(a, b int) (int, int) {
 	return a / b, a % b
 }
 
-func func_div(fc func(int, int) (int, int), c, d int) (int, int) {
+func funcDiv(fc func(int, int) (int, int), c, d int) (int, int) {
 	pointer := reflect.ValueOf(fc).Pointer()
 	funcName := runtime.FuncForPC(pointer).Name()
 	fmt.Printf("funcation with %s \n args=%d, \n args=%d \n", funcName, c, d)
@@ -140,7 +111,7 @@ func func_div(fc func(int, int) (int, int), c, d int) (int, int) {
 func argsFunc(args ...int) {
 	// 得到一个新的切片
 	args = append(args, 1888888)
-	fmt.Println("argsFunc函数调用")
+	fmt.Println("argsFunc 函数调用")
 	for _, arg := range args {
 		fmt.Println(arg)
 	}
