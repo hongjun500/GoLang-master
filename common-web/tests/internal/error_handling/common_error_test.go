38454652e6daef62df34a9ef5c6f5e69298705d4
hongjun500 <bhj.hongjun@gmail.com>
2024-06-13 23:36:32 +0800

    NewCommonErrorV2

--- common-web/tests/internal/error_handling/common_error_test.go
+++ common-web/tests/internal/error_handling/common_error_test.go
@@ -46,14 +46,14 @@ func TestCommonErrorOneArgument(t *testing.T) {
 
 func TestCommonErrorOneArgumentv2(t *testing.T) {
 	// panic
-	assert.Panics(t, func() { _ = error_handling.NewCommonErrorv2("test error_handling") })
+	assert.Panics(t, func() { _ = error_handling.NewCommonErrorV2("test error_handling") })
 
-	err := error_handling.NewCommonErrorv2(newerr)
+	err := error_handling.NewCommonErrorV2(newerr)
 	t.Logf("error_handling = %v", err)
 	assert.Equal(t, newerr, err.Err)
 	assert.Equal(t, "error_handling is not nil", err.Err.Error())
 
-	err = error_handling.NewCommonErrorv2(error_handling.UnknownError)
+	err = error_handling.NewCommonErrorV2(error_handling.UnknownError)
 	t.Logf("error_handling = %v", err)
 	assert.Nil(t, err.Err, "error_handling.Err is nil")
 	assert.Equal(t, error_handling.UnknownError, err.ErrCode)
@@ -78,15 +78,15 @@ func TestCommonErrorTwoArgument(t *testing.T) {
 }
 func TestCommonErrorTwoArgumentV2(t *testing.T) {
 	// panic
-	assert.Panics(t, func() { _ = error_handling.NewCommonErrorv2(newerr, "test error_handling") })
+	assert.Panics(t, func() { _ = error_handling.NewCommonErrorV2(newerr, "test error_handling") })
 
-	err := error_handling.NewCommonErrorv2(newerr, error_handling.ParameterValidationError)
+	err := error_handling.NewCommonErrorV2(newerr, error_handling.ParameterValidationError)
 	t.Logf("error_handling = %v", err)
 	assert.Equal(t, newerr, err.Err)
 	assert.Equal(t, error_handling.ParameterValidationError, err.ErrCode)
 	assert.Equal(t, "参数校验错误", err.ErrMsg, err.Error())
 
-	err = error_handling.NewCommonErrorv2(error_handling.UnknownError, "test error_handling")
+	err = error_handling.NewCommonErrorV2(error_handling.UnknownError, "test error_handling")
 	t.Logf("error_handling = %v", err)
 	assert.Nil(t, nil, err.Err)
 	assert.Equal(t, error_handling.UnknownError, err.ErrCode)
@@ -104,7 +104,7 @@ func TestCommonErrorThreeArgument(t *testing.T) {
 }
 
 func TestCommonErrorThreeArgumentV2(t *testing.T) {
-	err := error_handling.NewCommonErrorv2(newerr, error_handling.DatabaseError, "test database error_handling")
+	err := error_handling.NewCommonErrorV2(newerr, error_handling.DatabaseError, "test database error_handling")
 	t.Logf("error_handling = %s", err)
 	assert.Equal(t, newerr, err.Err)
 	assert.Equal(t, error_handling.DatabaseError, err.ErrCode)
